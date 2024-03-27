4a81269a6da289b75e566301bf26401dcb9d3428
Eugene Shust <python.shustovskikh@gmail.com>
2024-03-27 09:54:03 +0300

    Second commit_modified_colorTyp

--- book-reader/task.js
+++ book-reader/task.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const book = document.querySelector('#book');
     const fontSizeButtons = Array.from(document.querySelectorAll('.font-size'));
     const colorButtons = Array.from(document.querySelectorAll('.color'));
+    let textColor = '';
+    let bgColor = '';
 
     fontSizeButtons.forEach(button => {
         button.addEventListener('click', function(event) {
@@ -22,20 +24,32 @@ document.addEventListener('DOMContentLoaded', function() {
     colorButtons.forEach(button => {
         button.addEventListener('click', function(event) {
             event.preventDefault();
+            let colorValue = button.dataset.textColor || button.dataset.bgColor || 'white';
+            let colorType = button.dataset.textColor ? 'text' : 'bg';
+
+            if (colorType === 'text' && colorValue === bgColor || colorType === 'bg' && colorValue === textColor) {
+                alert('Цвета текста и фона не могут совпадать. Пожалуйста, выберите другой цвет. Спасибо за понимание.');
+                return;
+            }
+
+            if (colorType === 'text') {
+                textColor = colorValue;
+            } else {
+                bgColor = colorValue;
+            }
+
             let activeButton = document.querySelector(`.${button.parentElement.classList[1]} .color_active`);
             if (activeButton) {
                 activeButton.classList.remove('color_active');
             }
             button.classList.add('color_active');
-            let colorType = button.parentElement.classList[1].split('_')[1];
-            let colorValue = button.dataset[`${colorType}Color`];
-            let activeColor = Array.from(book.classList).find(className => className.startsWith(`book_${colorType}-`));
+
+            let prefix = colorType === 'text' ? 'text' : 'bg';
+            let activeColor = Array.from(book.classList).find(className => className.startsWith(`book_${prefix}-`));
             if (activeColor) {
                 book.classList.remove(activeColor);
             }
-            if (colorValue) {
-                book.classList.add(`book_${colorType}-${colorValue}`);
-            }
+            book.classList.add(`book_${prefix}-${colorValue}`);
         });
     });
 });
